418b5537771f61dd7a551b2105db35d9c88a8b5e
Aleksandr Zuev <zuev08@gmail.com>
2023-07-05 10:27:58 +0200

    run.sh on police-107 image

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+input_image=${1:-/dss/dsshome1/0D/ge24tav3/checkpoints/custom_images/police-107.jpg}
+
 export CUDA_VISIBLE_DEVICES=0
 python scene_graph_gen.py \
   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
   --grounded_checkpoint weights/groundingdino_swint_ogc.pth \
   --sam_checkpoint weights/sam_vit_h_4b8939.pth \
-  --input_image assets/demo.jpg \
+  --input_image "$input_image" \
   --output_dir "outputs" \
-  --box_threshold 0.25 \
-  --text_threshold 0.2 \
-  --iou_threshold 0.5  \
-  --device "cpu"
+  --box_threshold 0.15 \
+  --text_threshold 0.1 \
+  --iou_threshold 0.25  \
+  --device "cuda"
